c154af903ba5f7d6281c2590644b5e8e72682b6d
aldyahmad64 <aldyahmad64@gmail.com>
2025-03-13 20:09:44 +0700

    Penambahan fitur database notifikasi

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -53,6 +53,8 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
+            ->databaseNotifications()
+            ->databaseNotificationsPolling('15s')
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('test', function () {
+    \Filament\Notifications\Notification::make()
+        ->title('Judul')
+        ->body('Ini adalah body')
+        ->success()
+        ->actions([
+            \Filament\Notifications\Actions\Action::make('view')
+                ->button()
+                ->markAsRead(),
+        ])
+        ->sendToDatabase(\App\Models\User::role('operator')->get());
+});
